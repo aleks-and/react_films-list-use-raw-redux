5cf9798b060d8e5193174b5af6e4ff338c2163b5
Alex Andrutskii <alex.andrutskii@gmail.com>
2019-10-04 00:10:28 +0300

    fix

---
+++ build/precache-manifest.b387690629228624bb65facc88b7d8b1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "773d3ada4a0d031b88f06726aebffdde",
+    "url": "/react_films-list-use-raw-redux/index.html"
+  },
+  {
+    "revision": "c716cc6bfc919f6968f2",
+    "url": "/react_films-list-use-raw-redux/static/css/2.1ab5750d.chunk.css"
+  },
+  {
+    "revision": "efab9a65de5667921089",
+    "url": "/react_films-list-use-raw-redux/static/css/main.74eea0a1.chunk.css"
+  },
+  {
+    "revision": "c716cc6bfc919f6968f2",
+    "url": "/react_films-list-use-raw-redux/static/js/2.7f32a56a.chunk.js"
+  },
+  {
+    "revision": "efab9a65de5667921089",
+    "url": "/react_films-list-use-raw-redux/static/js/main.408ee58a.chunk.js"
+  },
+  {
+    "revision": "7588ac357f07406e5d63",
+    "url": "/react_films-list-use-raw-redux/static/js/runtime~main.1e9a4e9f.js"
+  }
+]);
